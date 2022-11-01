5eb0943128fcd9a61d9920cfa7e9a4d2211b0f6c
Corize Hoang Nguyen <corizehoangnguyen@Corizes-MacBook-Air.local>
2022-11-01 19:24:08 +0700

    refactor code

--- .history/src/models/sale-product.model_20220501034325.ts
+++
@@ -1,9 +0,0 @@
-import { Document, model, Schema, Types } from 'mongoose';
-const saleProductSchema: Schema = new Schema({
-  _id: Types.ObjectId,
-  product: { type: Types.ObjectId, ref: 'products' },
-  quantity: { type: Number, required: true },
-});
-const saleProductModel = model<ISAl & Document>('sale-product', saleProductSchema);
-
-export default saleProductModel;
